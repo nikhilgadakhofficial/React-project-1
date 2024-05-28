f92f17424cd82bd135bd05db10a089c2a2070d5d
nikhilgadakhofficial <nikhilgadakh543@gmail.com>
2024-05-28 19:05:28 +0530

    add 1

--- src/Components/Confg/Confg.js
+++ src/Components/Confg/Confg.js
@@ -46,48 +46,48 @@ const MANU = [
         category : "Non Veg",
         rs :" $ 350 ",
         description:"What is the most ordered food online? India's love for biryani continues Biryani continued its reign as India's favourite dish. It was the most-ordered dish on Swiggy for the eighth year in a rowWhat is the most ordered food online? India's love for biryani continues Biryani continued its reign as India's favourite dish. It was the most-ordered dish on Swiggy for the eighth year in a row.What is the most ordered food online? India's love for biryani continues Biryani continued its reign as India's favourite dish. It was the most-ordered dish on Swiggy for the eighth year in a row.",
-        img : "./img/img-1.webp"
+        img : "https://www.yudaah.com/demo/fast-food-website-template/assets/images/menu/1.jpg"
     },
     {
         id : 2,
-        title: "Anda Noodles",
+        title: "Italian Source Mushroom",
         category : "Non Veg",
-        rs :" $ 50 ",
-        description:"What is the most ordered food online? India's love for biryani continues Biryani continued its reign as India's favourite dish. It was the most-ordered dish on Swiggy for the eighth year in a row. India ordered 2.5 biryanis per second in 2023. And leading the charge was one Hyderabad man who .",
+        rs :" $ 550 ",
+        description:"What is mushroom sauce made of It is made with mushrooms, butter, cream or olive oil, white wine (some variations may use a mellow red wine) and pepper with a wide variety of variations possible with additional ingredients such as shallot, garlic, lemon juice, flour (to thicken the sauce), chicken stock, saffron, basil, parsley, or other herbs..",
         img : "https://www.yudaah.com/demo/fast-food-website-template/assets/images/menu/2.jpg"
     },
     {
         id : 3,
-        title: "Anda Noodles",
-        category : "Non Veg",
-        rs :" $ 30 ",
-        description:"What is the most ordered food online? India's love for biryani continues Biryani continued its reign as India's favourite dish. It was the most-ordered dish on Swiggy for the eighth year in a row. India ordered 2.5 biryanis per second in 2023. And leading the charge was one Hyderabad man who .",
-        img : "./img/img-1.webp"
+        title: "Fried Potatoes With Garlic",
+        category : "Veg",
+        rs :" $ 300 ",
+        description:"The best side dishes to serve with fried potatoes and onions are chicken wings, gravy, ratatouille, London broil, Mississippi pot roast, Tabasco, chicken meatballs, roasted asparagus, balsamic glazed carrots, fried green beans, bruschetta, deviled eggs, cucumber salad, stuffed mushrooms, and garlic knotsFried potatoes are a dish or a component of other dishes (such as Bauernfrühstück) essentially consisting of potatoes which have been fried or deep-fried in hot cooking oil often with the addition of salt and other seasonings. They are often served as a side dish..",
+        img : "https://www.yudaah.com/demo/fast-food-website-template/assets/images/menu/3.jpg"
     },
     {
         id : 4,
-        title: "Anda Noodles",
+        title: "Fried Chicken Salad",
         category : "Non Veg",
-        rs :" $ 30 ",
-        description:"What is the most ordered food online? India's love for biryani continues Biryani continued its reign as India's favourite dish. It was the most-ordered dish on Swiggy for the eighth year in a row. India ordered 2.5 biryanis per second in 2023. And leading the charge was one Hyderabad man who .",
-        img : "./img/img-1.webp"
+        rs :" $ 400 ",
+        description:"Fried chicken has been described as being crunchy and juicy, as well as crispy The dish has also been called spicy and salty. Occasionally, fried chicken is also topped with chili like paprika, or hot sauce to give it a spicy taste. This is especially common in fast food restaurant chains such as KFCFried chicken has been described as being crunchy and juicy, as well as crispy The dish has also been called spicy and salty. Occasionally, fried chicken is also topped with chili like paprika, or hot sauce to give it a spicy taste. This is especially common in fast food restaurant chains such as KFC. .",
+        img : "https://www.yudaah.com/demo/fast-food-website-template/assets/images/menu/4.jpg"
     },
     
     {
         id : 5,
-        title: "Anda Noodles",
+        title: "Fresh Crab With Lemon",
         category : "Non Veg",
-        rs :" $ 30 ",
-        description:"What is the most ordered food online? India's love for biryani continues Biryani continued its reign as India's favourite dish. It was the most-ordered dish on Swiggy for the eighth year in a row. India ordered 2.5 biryanis per second in 2023. And leading the charge was one Hyderabad man who .",
-        img : "./img/img-1.webp"
+        rs :" $ 730 ",
+        description:"A crab that has recently moulted will have a very clean, shiny and intact shell. Sometimes the shell will appear translucent. Full mud crabs often have algae or barnacles on the shell. Their shells are also likely to exhibit some general signs of wear and tear, such as worn inner 'teeth' on the claws.Fresh crab meat should not have any strong odors. In fact, it usually doesn't smell of much, except for a mild sweet odor. If you notice the meat having a sour, rotting, or bitter smell, this is an indication that the crab meat has gone bad, and is not fit for consumption.",
+        img : "https://www.yudaah.com/demo/fast-food-website-template/assets/images/menu/5.jpg"
     },
     {
         id : 6,
-        title: "Anda Noodles",
+        title: "Grilled Salmon Sushe",
         category : "Non Veg",
-        rs :" $ 30 ",
-        description:"What is the most ordered food online? India's love for biryani continues Biryani continued its reign as India's favourite dish. It was the most-ordered dish on Swiggy for the eighth year in a row. India ordered 2.5 biryanis per second in 2023. And leading the charge was one Hyderabad man who .",
-        img : "./img/img-1.webp"
+        rs :" $ 390 ",
+        description:"Salmon harasu (belly) is the counterpart to tuna's toro, or the fatty cut from the belly of the fish. Salmon belly is incredibly rich and also an uncommon menu item, so you're more likely to find it in a proper sushi restaurant rather than a kaiten-zushi shop",
+        img : "https://www.yudaah.com/demo/fast-food-website-template/assets/images/menu/6.jpg"
     }
 ]
 
@@ -98,19 +98,19 @@ const TEAM = [
         titile : "Founder & CEO"
     },
     {
-        img : "https://www.yudaah.com/demo/fast-food-website-template/assets/images/team/1.jpg",
-        name: "Mark Anthony",
-        titile : "Founder & CEO"
+        img : "https://www.yudaah.com/demo/fast-food-website-template/assets/images/team/2.jpg",
+        name: "Jessica Lee",
+        titile : "Chinese Kitchen Lead"
     },
     {
-        img : "https://www.yudaah.com/demo/fast-food-website-template/assets/images/team/1.jpg",
-        name: "Mark Anthony",
-        titile : "Founder & CEO"
+        img : "https://www.yudaah.com/demo/fast-food-website-template/assets/images/team/3.jpg",
+        name: "John Bennett",
+        titile : "French Kitchen Lead"
     },
     {
-        img : "https://www.yudaah.com/demo/fast-food-website-template/assets/images/team/1.jpg",
-        name: "Mark Anthony",
-        titile : "Founder & CEO"
+        img : "https://www.yudaah.com/demo/fast-food-website-template/assets/images/team/4.jpg",
+        name: "ANDERSON JHON",
+        titile : "Sous Chef"
     }
 ]
 export {DATA ,ADD , MANU , TEAM}
--- src/Components/TeamCard/TeamCard.js
+++ src/Components/TeamCard/TeamCard.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import './TeamCard.css'
+import ImgTame1 from './../../Views/Home/phone-call (1).png'
+import ImgTame2 from './../../Views/Home/facebook (2).png'
+import ImgTame3 from './../../Views/Home/instagram (1).png'
 function TeamCard({ titile , img , name}) {
   return (
     <div className='tame-card'>
         <img className='tame-img' src={img}/>
         <h1 className='team-name'>{name}</h1>
         <p className='team-titlel'>{titile}</p>
+         <div className='img-card'>
+        <img className='in-img' src={ImgTame1}/>
+        <img className='in-img' src={ImgTame2}/>
+        <img className='in-img' src={ImgTame3}/>
+        </div>
     </div>
   )
 }
